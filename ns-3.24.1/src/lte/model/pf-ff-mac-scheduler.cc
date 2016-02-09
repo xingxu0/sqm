7527fcd0912d8e44d74cf68fd31ba1d339a88d38
xingxux <vess527@gmail.com>
2016-02-08 19:57:02 -0800

    a

--- ns-3.24.1/src/lte/model/pf-ff-mac-scheduler.cc
+++ ns-3.24.1/src/lte/model/pf-ff-mac-scheduler.cc
@@ -1163,6 +1163,8 @@ PfFfMacScheduler::DoSchedDlTriggerReq (const struct FfMacSchedSapProvider::Sched
                       // this UE has data to transmit
                       double achievableRate = 0.0;
                       uint8_t mcs = 0;
+
+		      //std::cout<<(int)(*it).first<<" "<<nLayer<<" "<<(int)rbgSize<<"     ";
                       for (uint8_t k = 0; k < nLayer; k++)
                         {
                           if (sbCqi.size () > k)
@@ -1175,8 +1177,12 @@ PfFfMacScheduler::DoSchedDlTriggerReq (const struct FfMacSchedSapProvider::Sched
                               mcs = 0;
                             }
 			  // achievableRate is Tmax in Magus slides
+			  //std::cout<<(int)mcs<<" ";
                           achievableRate += ((m_amc->GetTbSizeFromMcs (mcs, rbgSize) / 8) / 0.001);   // = TB size / TTI
                         }
+		      (*rnti_mcs)[(*it).first] = mcs;
+		      (*rnti_rate)[(*it).first] = achievableRate;
+		      //std::cout<<achievableRate<<std::endl;
 			//Xing
                       //double rcqi = achievableRate / (*it).second.lastAveragedThroughput;
 		      double rcqi;
--- ns-3.24.1/src/lte/model/weight-table.h
+++ ns-3.24.1/src/lte/model/weight-table.h
@@ -3,3 +3,7 @@
 std::map <uint64_t, uint16_t> * imsi_id;
 std::map <uint16_t, uint64_t> * rnti_imsi;
 std::map <uint16_t, float> * id_weight;
+
+std::map <uint16_t, uint8_t> * rnti_mcs;
+std::map <uint16_t, double> * rnti_rate;
+
