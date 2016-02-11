66dc6d62884e4c12c40c0e9abbf42fa7fa0079fd
xingxux <vess527@gmail.com>
2016-02-10 17:02:14 -0800

    a

--- ns-3.24.1/captcp.bash
+++ ns-3.24.1/captcp.bash
@@ -1,8 +1,8 @@
-for i in {0..11}
+for i in 0
 do
   captcp throughput -s 0.3 -i -f 1.1 -o cap dynamic_weight_$i.pcap
   cd cap
   make png
-  cp throughput.png ~/Dropbox/$i\_requirement.png
+  cp throughput.png ~/Dropbox/new_mobility_$i.png
   cd ..
 done
