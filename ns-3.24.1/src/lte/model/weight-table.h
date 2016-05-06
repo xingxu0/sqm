1a2c8a57c6501fb98349aaeb9031877de1aa40a1
xingxux <vess527@gmail.com>
2016-05-06 10:50:48 -0700

    a

--- ns-3.24.1/src/lte/model/weight-table.h
+++ ns-3.24.1/src/lte/model/weight-table.h
@@ -10,6 +10,7 @@ std::map <uint16_t, uint16_t> * rnti_mcs_count;
 std::map <uint16_t, double> * rnti_rate;
 
 std::map <uint16_t, uint16_t> * rnti_prbs;
+std::map <uint16_t, uint16_t> * rnti_prbs_previous;
 
 std::map <uint16_t, uint32_t> * rnti_mcs_selected;
 std::map <uint16_t, uint16_t> * rnti_mcs_count_selected;
@@ -26,6 +27,7 @@ void init() {
 	rnti_mcs_count = new std::map <uint16_t, uint16_t>();
 	rnti_rate = new std::map<uint16_t, double>();
 	rnti_prbs = new std::map<uint16_t, uint16_t>();
+	rnti_prbs_previous = new std::map<uint16_t, uint16_t>();
 	
 	// mcs value when this user got selected
 	rnti_mcs_selected = new std::map <uint16_t, uint32_t>();
